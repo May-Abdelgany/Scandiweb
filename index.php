91832f647389248352391770dccb7bfbcba825a4
May-Abdelghany <mayawaad622@gmail.com>
2022-04-04 05:27:33 +0200

    update

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-header("Location:product.php");
-?>
-<head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,7 +10,9 @@ header("Location:product.php");
 </head>
 
 <body>
-     
+     <script>
+         window.location.href="product.php";
+     </script> 
     <script src="js/jquery-1.11.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/index.js"></script>
